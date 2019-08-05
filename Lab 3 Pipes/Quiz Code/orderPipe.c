42b539f0314009cbdfdd269644f9dbc7f159358b
Jonathan <JonathanMichaelEdwards>
2019-08-05 22:30:04 +1200

    lab 3

---
+++ Lab 3 Pipes/Quiz Code/orderPipe.c
@@ -0,0 +1,44 @@
+/* Title: pipedup2.c
+ * Description: ENCE360 Example code - dup2.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define INP 1
+#define OUTP 0
+
+int main(int argc, char const *argv[]) 
+{
+    char inbuf[BUFSIZ];
+    int fd[2];
+    pid_t childpid;
+
+    pipe(fd);
+    if ((childpid = fork()) == 0) { /* Child code: Runs ls */
+        dup2(fd[INP], STDOUT_FILENO);
+        close(fd[OUTP]);
+        close(fd[INP]);
+        execl("/bin/ls", "ls", "-l", NULL);
+        perror("The exec of ls failed");
+    }
+
+    else { /* Parent code: Runs sort */
+        dup2(fd[OUTP], STDIN_FILENO);
+        close(fd[OUTP]);
+        close(fd[INP]);
+
+        
+
+        printf("hello %s\n", argv[0]);
+        // execl("/usr/bin/sort", "sort", "-k", "+8", NULL);
+        execl("/usr/bin/head", "-l", NULL);
+        /* Note: The location of sort depends on your distribution.
+         * Use 'which sort' to find the correct location */
+        perror("The exec of sort failed");
+    }
+
+    exit(0);
+}
